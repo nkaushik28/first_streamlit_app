4f8fd3ed9b652ecaa73e93e274e181a531975f8b
Nishant <109677065+nkaushik28@users.noreply.github.com>
2022-07-20 11:24:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 streamlit.title("My Mom's New Healthy Diner")
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
